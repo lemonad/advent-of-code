555ffaac94bdb16626de9364ba33622599c7341c
Jonas Nockert <jonasnockert@gmail.com>
2017-12-30 18:36:03 +0100

    Flake8 passes now

--- 2015 (Python)/tests/test_dec06.py
+++ 2015 (Python)/tests/test_dec06.py
@@ -6,23 +6,24 @@ from december06 import Solver
 class TestDec6(unittest.TestCase):
 
     def test_examples_for_part_one(self):
-        on = Solver(from_str='turn on 0,0 through 999,999').solve_part_one()
-        self.assertEqual(on, 1000000)
-        on = Solver(from_str='toggle 0,0 through 999,0').solve_part_one()
-        self.assertEqual(on, 1000)
-        on = Solver(from_str='toggle 0,0 through 999,0\n'
-                             'toggle 0,0 through 999,0').solve_part_one()
-        self.assertEqual(on, 0)
-        on = Solver(from_str='turn on 0,0 through 999,999\n'
-                             'turn off 499,499 through 500,500').solve_part_one()
-        self.assertEqual(on, 1000000 - 4)
+        s = 'turn on 0,0 through 999,999'
+        self.assertEqual(Solver(from_str=s).solve_part_one(), 1000000)
+
+        s = 'toggle 0,0 through 999,0'
+        self.assertEqual(Solver(from_str=s).solve_part_one(), 1000)
+
+        s = 'toggle 0,0 through 999,0\ntoggle 0,0 through 999,0'
+        self.assertEqual(Solver(from_str=s).solve_part_one(), 0)
+
+        s = 'turn on 0,0 through 999,999\nturn off 499,499 through 500,500'
+        self.assertEqual(Solver(from_str=s).solve_part_one(), 1000000 - 4)
 
     def test_examples_for_part_two(self):
-        brightness = Solver(from_str='turn on 0,0 through 0,0').solve_part_two()
-        self.assertEqual(brightness, 1)
-        brightness = Solver(from_str='toggle 0,0 through 999,999')\
-                     .solve_part_two()
-        self.assertEqual(brightness, 2000000)
+        s = 'turn on 0,0 through 0,0'
+        self.assertEqual(Solver(from_str=s).solve_part_two(), 1)
+
+        s = 'toggle 0,0 through 999,999'
+        self.assertEqual(Solver(from_str=s).solve_part_two(), 2000000)
 
     def test_solution(self):
         (one, two) = Solver(from_file='input/dec06.in').solve()
