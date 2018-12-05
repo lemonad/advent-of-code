db40171325527ac909b845c537349d78dc701917
Jonas Nockert <jonasnockert@gmail.com>
2018-12-05 22:22:58 +0100

    Found smarter regex for dec 5

--- 2018 (Python)/december05.py
+++ 2018 (Python)/december05.py
@@ -3,6 +3,10 @@ December 05, Advent of Code 2018 (Jonas Nockert / @lemonad)
 
 This is an obviously dumb solution (but also quite short and straightforward :)
 
+Edit: Aha, the way to match Aa and aA but not AA with regex is to search
+for two consecutive characters that are different but same when compared
+without regard to case.
+
 """
 import re
 
@@ -13,11 +17,12 @@ class Solver(PuzzleSolver):
     def __init__(self, *args, **kwargs):
         super(Solver, self).__init__(*args, **kwargs)
         # Generate regex for "(aA|...|zZ|Aa|...|Zz)".
-        pairs = (
-            "{0}{1}|{1}{0}".format(chr(c), chr(c).upper())
-            for c in range(ord("a"), ord("z") + 1)
-        )
-        self.rereact = re.compile("|".join(pairs))
+        # pairs = (
+        #     "{0}{1}|{1}{0}".format(chr(c), chr(c).upper())
+        #     for c in range(ord("a"), ord("z") + 1)
+        # )
+        # self.rereact = re.compile("|".join(pairs))
+        self.rereact = re.compile(r"(.)(?!\1)(?i:\1)")
 
     def react(self, polymer):
         n = 1
